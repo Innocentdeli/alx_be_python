e508686fbd4512abcac68751bddff65656d09aaf
Innocentdeli <innocentdeli1@gmail.com>
2025-07-17 19:06:10 -0700

    new tasks

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = input('How old are you? ')
+age = int(input('How old are you? '))
 current_year = 2023
 future_age = age + (2050 - 2023)
 print('In 2050, you will be ', future_age, ' years old.')
