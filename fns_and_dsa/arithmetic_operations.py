90531537c74b7db961f61019735f62986e2852b6
Innocentdeli <innocentdeli1@gmail.com>
2025-08-06 22:42:08 -0700

    create operation calculator

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
-	if operation == +:
+	if operation == add:
 		result = num1 = num2
 		return result
-	elif operation == -:
+	elif operation == subtract:
 		result = num1 - num2
 		return result
-	elif operation == *:
+	elif operation == multiply:
 		result = num1 * num2
 		return result
-	elif operation == /:
+	elif operation == divide:
 		if num2 != 0:
 			result = num1 / num2
 			return result
