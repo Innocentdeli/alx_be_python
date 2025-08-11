5b43a47d18d5554af51adfa5048f28f41f2e83ed
Innocentdeli <innocentdeli1@gmail.com>
2025-08-11 12:24:46 -0700

    updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
-	if operation == add:
+	if operation == 'add':
 		result = num1 = num2
 		return result
-	elif operation == subtract:
+	elif operation == 'subtract':
 		result = num1 - num2
 		return result
-	elif operation == multiply:
+	elif operation == 'multiply':
 		result = num1 * num2
 		return result
-	elif operation == divide:
+	elif operation == 'divide':
 		if num2 != 0:
 			result = num1 / num2
 			return result
