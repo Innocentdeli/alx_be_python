99e30ff0e6246da99e0ea98caae10180f254a248
Innocentdeli <innocentdeli1@gmail.com>
2025-08-15 10:06:58 -0700

    Writing Unit Tests for a Simple Calculator Class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,37 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_divide(self):
+        # Normal division
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333, places=6)
+
+        # Division by zero
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsNone(self.calc.divide(0, 0))
+
+if __name__ == "__main__":
+    unittest.main()
+
