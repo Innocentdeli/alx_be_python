62d2d7b466875867352bbeae6b2962ed2d911249
Innocentdeli <innocentdeli1@gmail.com>
2025-08-16 00:31:39 -0700

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,10 +1,18 @@
 class Book:
-	def __init__(self, title:str, author:str, year:int):
-		self.title = title
-		self.author = author
-		self.year = year
-	def __del__(self):
-		print(f'Deleting {self.title}')
-	def __str__(self):
-		print(f'{self.title} by {self.author}, published in {self.year}'	def __repr__(self):
-		return(f"Book('{self.title}', '{self.author}', {self.year})")
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        """Return a user-friendly string representation of the book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Return the official string representation of the book."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        """Print a message when a book is deleted."""
+        print(f"Deleting {self.title}")
+
